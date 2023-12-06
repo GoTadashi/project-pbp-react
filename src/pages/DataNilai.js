ae80d1c7ad0269571240f1b967ce078203af2771
UncleJoo16 <jonathansatria16@gmail.com>
2023-12-06 10:58:36 +0700

    asd

--- src/pages/DataNilai.js
+++ src/pages/DataNilai.js
@@ -13,7 +13,7 @@ export const DataNilai = () => {
         // const response = await fetch(
         //   "http://jojopinjam.iffan.site/api/get-siswa"
         // );
-        const response = await fetch("http://127.0.0.1:8000/api/get-siswa");
+        const response = await fetch("https://jojopinjam.iffan.site/api/get-siswa");
         const dataSiswa = await response.json();
         setDataSiswa(dataSiswa);
       } catch (error) {
