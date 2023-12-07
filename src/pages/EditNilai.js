07fdd06b3e7c8bb3e8dcf9fbc9c07b89494de740
UncleJoo16 <jonathansatria16@gmail.com>
2023-12-07 22:10:21 +0700

    editnilai

--- src/pages/EditNilai.js
+++ src/pages/EditNilai.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Link } from "react-router-dom";
@@ -6,17 +7,205 @@ import "./EditNilai.css";
 import mainLogo from "../img/logo.png";
 
 export const EditNilai = () => {
+  const { nisSiswa } = useParams();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [dataSiswa, setDataSiswa] = useState([]);
+  const [dataMapel, setDataMapel] = useState([]);
+  const [dataNilai, setDataNilai] = useState("");
+  const [dataPredikat, setDataPredikat] = useState("");
+  const [dataDeskripsi, setDataDeskripsi] = useState("");
+  const [selectedSiswa, setSelectedSiswa] = useState(nisSiswa);
+  const [selectedMapel, setSelectedMapel] = useState("");
+  const [selectedIdRaport, setSelectedIdRaport] = useState("");
+  const [raportData, setRaportData] = useState([]);
 
-  const [namaSiswa, setNamaSiswa] = useState("");
-  const [mataPelajaran, setMataPelajaran] = useState("");
-  const [semester, setSemester] = useState("");
-  const [nilai, setNilai] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responseSiswa = await fetch(
+          `https://jojopinjam.iffan.site/api/get-siswa/${nisSiswa}`
+        );
+        const dataSiswa = await responseSiswa.json();
 
-  const handleSubmit = (e) => {
+        if (Array.isArray(dataSiswa)) {
+          setDataSiswa(dataSiswa);
+        } else if (dataSiswa) {
+          setDataSiswa([dataSiswa]);
+        } else {
+          console.error("Invalid Data Siswa format:", dataSiswa);
+          setDataSiswa([]);
+        }
+
+        // Check if there is valid data for the student
+        if (dataSiswa.length > 0) {
+          const studentId = dataSiswa[0].id; // Assuming id is the correct property
+          const responseRaport = await fetch(
+            `https://jojopinjam.iffan.site/api/get-raport-main/${studentId}`
+          );
+          const raportData = await responseRaport.json();
+
+          console.log("Raport data:", raportData);
+
+          if (raportData.length > 0) {
+            const firstRaportId = raportData[0].id_raport; // Assuming id_raport is the correct property
+            setSelectedIdRaport(firstRaportId);
+          } else {
+            console.error("No raport data available for the selected student.");
+          }
+        } else {
+          console.error("No data available for the selected student.");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [nisSiswa]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jojopinjam.iffan.site/api/get-matapelajaran"
+        );
+        const dataMapel = await response.json();
+        setDataMapel(dataMapel);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchRaportData = async () => {
+      try {
+        console.log("Fetching raport data...");
+        const response = await fetch(
+          `https://jojopinjam.iffan.site/api/get-raport-main/${nisSiswa}`
+        );
+        const raportData = await response.json();
+
+        console.log("Raport data:", raportData);
+        setRaportData(raportData);
+
+        if (Array.isArray(raportData) && raportData.length > 0) {
+          setSelectedIdRaport(raportData.map((raport) => raport.id_raport));
+        } else {
+          console.error("No raport data found for the selected student.");
+          setSelectedIdRaport([]);
+        }
+      } catch (error) {
+        console.error("Error fetching raport data:", error);
+      }
+    };
+
+    if (nisSiswa) {
+      fetchRaportData();
+    }
+  }, [nisSiswa]);
+
+  const addNilai = async () => {
+    // Validate the input fields
+    if (!selectedMapel || !selectedIdRaport || !dataNilai) {
+      console.error("Please fill in all the required fields.");
+      return {
+        success: false,
+        message: "Please fill in all the required fields.",
+      };
+    }
+
+    // Konversi dataNilai dari string ke number jika perlu
+    const nilai = parseInt(dataNilai);
+    let dataPredikat = "";
+    let dataDeskripsi = "";
+
+    if (nilai > 90 && nilai <= 100) {
+      dataPredikat = "A";
+      dataDeskripsi = "Sangat Baik";
+    } else if (nilai > 80) {
+      dataPredikat = "B";
+      dataDeskripsi = "Baik";
+    } else if (nilai > 70) {
+      dataPredikat = "C";
+      dataDeskripsi = "Cukup";
+    } else if (nilai > 60) {
+      dataPredikat = "D";
+      dataDeskripsi = "Kurang";
+    } else {
+      dataPredikat = "E";
+      dataDeskripsi = "Sangat Kurang";
+    }
+
+    const newNilai = {
+      // nis: selectedSiswa,
+      id_matapelajaran: selectedMapel,
+      id_raport: parseInt(selectedIdRaport),
+      nilai: nilai, // Gunakan variabel 'nilai' yang sudah diubah menjadi number
+      predikat: dataPredikat,
+      deskripsi: dataDeskripsi,
+    };
+
+    try {
+      const response = await fetch(
+        "https://jojopinjam.iffan.site/api/add-detail",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newNilai),
+        }
+      );
+
+      const data = await response.json();
+
+      console.log("Nilai added successfully:", data);
+      // Additional actions after successfully adding the nilai
+
+      return { success: true, message: "Data berhasil dimasukkan!" };
+    } catch (error) {
+      console.error("Error adding nilai:", error);
+
+      return { success: false, message: "Terjadi kesalahan. Mohon coba lagi." };
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Add your logic for form submission here, e.g., call an API
-    console.log("Submitted:", namaSiswa, mataPelajaran, semester, nilai);
+    try {
+      const response = await addNilai();
+      console.log(
+        "Submitted:",
+        selectedMapel,
+        selectedIdRaport,
+        parseInt(dataNilai),
+        dataPredikat,
+        dataDeskripsi
+      );
+
+      if (response.success) {
+        alert("Data berhasil dimasukkan!");
+      } else {
+        alert(
+          "Data tidak berhasil dimasukkan. Mohon coba lagi. " + response.message
+        );
+      }
+    } catch (error) {
+      console.error("Error submitting data:", error);
+      alert("Terjadi kesalahan. Mohon coba lagi.");
+    }
+  };
+
+  const handleReset = () => {
+    // setSelectedMapel(""),
+    //   setSelectedIdRaport(""),
+    //   setDataNilai(""),
+    //   setDataPredikat(""),
+    //   setDataDeskripsi("");
   };
 
   return (
@@ -147,83 +336,88 @@ export const EditNilai = () => {
           <i class="bi bi-arrow-left fs-6 text-white"></i>
           <div className="text-wrapper-11">Kembali</div>
         </div>
-        <div className="text-wrapper-12">Input Nilai</div>
-        <form onSubmit={handleSubmit}>
-          <div className="group-8">
-            <div className="frame-wrapper">
-              <div className="frame-2">
-                <div className="text-wrapper-13">Nama Siswa</div>
-              </div>
-            </div>
-            <div className="frame-3">
-              <input
-                type="text"
-                className="custom-input"
-                value={namaSiswa}
-                onChange={(e) => setNamaSiswa(e.target.value)}
-              />
+        <div className="text-wrapper-12">Edit Nilai</div>
+        <div className="group-8">
+          <div className="frame-wrapper">
+            <div className="frame-2">
+              <div className="text-wrapper-13">Nama Siswa</div>
             </div>
           </div>
-
-          <div className="group-9">
-            <div className="frame-wrapper">
-              <div className="frame-2">
-                <div className="text-wrapper-13">Mata Pelajaran</div>
-              </div>
-            </div>
-            <div className="frame-4">
-              <input
-                type="text"
-                className="custom-input"
-                value={mataPelajaran}
-                onChange={(e) => setMataPelajaran(e.target.value)}
-              />
+          <div className="frame-2">
+            <select
+              className="dropdown"
+              onChange={(e) => setSelectedSiswa(e.target.value)}
+            >
+              {dataSiswa.map((item, index) => (
+                <option key={index} value={item.nis}>
+                  {item.nama}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="group-9">
+          <div className="frame-2">
+            <div className="text-wrapper-13">Mata Pelajaran</div>
+          </div>
+          <div className="frame-2">
+            <select
+              className="dropdown"
+              onChange={(e) => setSelectedMapel(e.target.value)}
+            >
+              {dataMapel.map((item) => (
+                <option
+                  key={item.id_matapelajaran}
+                  value={item.id_matapelajaran}
+                >
+                  {item.nama_matapelajaran}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="group-10">
+          <div className="frame-wrapper">
+            <div className="frame-2">
+              <div className="text-wrapper-13">Nilai</div>
             </div>
           </div>
-
-          <div className="group-10">
-            <div className="frame-wrapper">
-              <div className="frame-2">
-                <div className="text-wrapper-13">Semester</div>
-              </div>
-            </div>
-            <div className="frame-5">
-              <input
-                type="number"
-                className="custom-input"
-                value={semester}
-                onChange={(e) => setSemester(e.target.value)}
-                min="1"
-              />
+          <div className="frame-2">
+            <input
+              type="number"
+              className="dropdown"
+              onChange={(e) => setDataNilai(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <div className="group-11">
+          <div className="frame-wrapper">
+            <div className="frame-2">
+              <div className="text-wrapper-13">Id Raport</div>
             </div>
           </div>
-
-          <div className="group-11">
-            <div className="frame-wrapper">
-              <div className="frame-2">
-                <div className="text-wrapper-13">Nilai</div>
-              </div>
-            </div>
-            <div className="frame-6">
-              <input
-                type="number"
-                className="custom-input"
-                value={nilai}
-                onChange={(e) => setNilai(e.target.value)}
-              />
-            </div>
+          <div className="frame-2">
+            <select
+              className="dropdown"
+              value={selectedIdRaport} // Mengatur nilai yang dipilih dalam dropdown
+              onChange={(e) => setSelectedIdRaport(e.target.value)}
+            >
+              {raportData.map((item) => (
+                <option key={item.id_raport} value={item.id_raport}>
+                  {item.id_raport}
+                </option>
+              ))}
+            </select>
           </div>
-
-          <div className="group-12">
-            <div className="frame-7">
-              <div className="text-wrapper-15">Simpan</div>
-            </div>
-            <div className="frame-8">
-              <div className="text-wrapper-15">Reset</div>
-            </div>
+        </div>
+        <div className="group-12">
+          <div className="frame-7" onClick={handleSubmit}>
+            <div className="text-wrapper-15">Simpan</div>
           </div>
-        </form>
-
+          <div className="frame-8" onClick={handleReset}>
+            <div className="text-wrapper-15">Reset</div>
+          </div>
+        </div>
       </div>
     </div>
   );
