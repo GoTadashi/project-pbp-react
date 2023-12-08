43f76646ae5096bb2617b68929b0f503d9c69232
UncleJoo16 <jonathansatria16@gmail.com>
2023-12-08 12:41:22 +0700

    awd

--- src/App.js
+++ src/App.js
@@ -55,11 +55,6 @@ function App() {
           {/* Data Mapel */}
           <Route path="/TambahMapel" exact component={TambahMapel} />
           <Route path="/EditMapel/:id_matapelajaran" exact component={EditMapel} />
-          <Route
-            path="/EditMapel/:id_matapelajaran"
-            exact
-            component={EditMapel}
-          />
           <Route path="/DaftarMapel" exact component={DaftarMapel} />
 
           {/* Data Siswa */}
