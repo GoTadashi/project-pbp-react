a6fa2ccc5d4c4ef676f290e7ba64ae18212324ef
meytanggrn <meytanggrn@gmail.com>
2023-12-08 13:59:09 +0700

    y

--- src/pages/SiswaLihatNilai.js
+++ src/pages/SiswaLihatNilai.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useHistory, Link } from "react-router-dom";
 import "./SiswaLihatNilai.css";
+import mainLogo from "../img/logo.png";
 
 const SiswaLihatNilai = () => {
   const history = useHistory();
@@ -79,60 +80,40 @@ const SiswaLihatNilai = () => {
       <div className="div">
         <div className="MENU-SIDEBAR">
           <div className="PROFILE">
-            <img className="element" alt="Element" src="64.svg" />
-            <div className="text-wrapper">Pengaturan Profil</div>
+            <Link to="/LihatNilai" className="list-menu nav-link text-white fs-5">
+              <i class="bi bi-file-earmark-plus"></i>
+              <span className="side-text ms-2">Lihat Nilai</span>
+            </Link>
           </div>
           <div className="PROFILE-2">
-            <img className="element" alt="Element" src="image.svg" />
-            <div className="text-wrapper">Log Out</div>
-          </div>
-          <div className="group">
-            <div className="overlap-group">
-              <div className="overlap">
-                <div className="group-2">
-                  <div className="text-wrapper-2">Light</div>
-                  <img
-                    className="sun-solid"
-                    alt="Sun solid"
-                    src="sun-solid.svg"
-                  />
-                </div>
-              </div>
-              <div className="text-wrapper-3">Dark</div>
-              <img
-                className="moon-solid"
-                alt="Moon solid"
-                src="moon-solid.svg"
-              />
-            </div>
+            <Link to="/LoginAdmin" className="list-menu nav-link text-white fs-5">
+              <i className="bi bi-box-arrow-right"></i>
+              <span className="side-text ms-2">Log Out</span>
+            </Link>
           </div>
           <div className="NILAI-wrapper">
             <div className="NILAI">
-              <img
-                className="icon-SISWA"
-                alt="Icon SISWA"
-                src="icon-SISWA.png"
-              />
-              <div className="text-wrapper">Nilai</div>
+              <Link to="/DataSiswa" className="list-menu nav-link text-white fs-5">
+                <i className="bi bi-person-vcard"></i>
+                <span className="side-text ms-2">Siswa</span>
+              </Link>
             </div>
           </div>
           <div className="CHAT">
-            <img
-              className="icon-envelope"
-              alt="Icon envelope"
-              src="icon-envelope.png"
-            />
-            <div className="text-wrapper">Chat</div>
+            <Link to="/LihatGuru" className="list-menu nav-link text-white fs-5">
+              <i class="bi bi-person-video3"></i>
+              <span className="side-text ms-2">Guru</span>
+            </Link>
           </div>
           <div className="DASHBOARD">
-            <img className="element" alt="Element" src="1.svg" />
-            <div className="text-wrapper">Dashboard</div>
+            <Link to="/DashboardGuru" className="list-menu nav-link text-white fs-5">
+              <i className="bi bi-speedometer2"></i>
+              <span className="side-text ms-2">Dashboard</span>
+            </Link>
           </div>
         </div>
         <footer className="FOOTER">
           <p className="p">SD Kristen Terang Bangsa</p>
-          <img className="line" alt="Line" src="line-2.svg" />
-          <div className="text-wrapper-4">SCH</div>
         </footer>
         <div className="overlap-2">
           {raportData.map((item, index) => (
@@ -177,9 +158,8 @@ const SiswaLihatNilai = () => {
               {Array.from({ length: totalPages }, (_, index) => (
                 <div
                   key={index}
-                  className={`overlap-group-2 ${
-                    currentPage === index + 1 ? "active" : ""
-                  }`}
+                  className={`overlap-group-2 ${currentPage === index + 1 ? "active" : ""
+                    }`}
                   onClick={() => paginate(index + 1)}
                 >
                   <div className="ellipse" />
@@ -213,31 +193,24 @@ const SiswaLihatNilai = () => {
               <div className="group-3">
                 <div className="group-4">
                   <div className="group-5">
-                    <div className="text-wrapper-25">Siswa</div>
-                    <div className="text-wrapper-26">Anggi Adinda</div>
                   </div>
                 </div>
               </div>
             </div>
             <div className="element-wrapper">
-              <img className="element-2" alt="Element" src="3.svg" />
+              {/* <img className="element-2" alt="Element" src="3.svg" /> */}
             </div>
             <div className="search">
               <div className="group-6">
-                <img className="element-3" alt="Element" src="7.svg" />
+                {/* <img className="element-3" alt="Element" src="7.svg" /> */}
               </div>
             </div>
             <div className="SMP-KRISTEN-GETASAN">
-              Smp Kristen Getasan
+              SD Kristen Terang Bangsa
               <br />
-              e-rapor
+              E-Rapor
             </div>
-            <img className="ellipse-2" alt="Ellipse" src="ellipse-1192.svg" />
-            <img
-              className="SMP-KRISTEN"
-              alt="Smp KRISTEN"
-              src="SMP-KRISTEN-2.png"
-            />
+            <img className="img-logo" src={mainLogo} alt="logo-sd" />
           </div>
         </header>
       </div>
